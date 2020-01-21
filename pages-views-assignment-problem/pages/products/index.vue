33b2a358e91e80083fd8e992ecd626fc07404475
Artis3n <dev@artis3nal.com>
2020-01-21 18:22:15 -0500

    Assignment 1

---
+++ pages-views-assignment-problem/pages/products/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="container">
+    <nuxt-link to="/products/1">Product 1</nuxt-link>
+    <nuxt-link to="/products/2">Product 2</nuxt-link>
+  </section>
+</template>
+
+<style>
+  a {
+    margin: 1px auto;
+  }
+</style>
