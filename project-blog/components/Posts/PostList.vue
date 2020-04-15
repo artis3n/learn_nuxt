ac9c57838f6efdc334a1a79f8896dfbbdafa31cb
Artis3n <dev@artis3nal.com>
2020-04-14 23:10:14 -0400

    Working on blog project. Turning posts into components

---
+++ project-blog/components/Posts/PostList.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="post-list">
+    <post-preview
+      id="1"
+      thumbnail="https://s14-eu5.startpage.com/cgi-bin/serveimage?url=https%3A%2F%2Fi.ytimg.com%2Fvi%2F_1GLCEPezxk%2Fmaxresdefault.jpg&sp=3ac875bad39b3263c14e485a37d539e5&anticache=720150"
+      title="Hello There!"
+      preview-text="This is my first post."/>
+    <post-preview
+      id="2"
+      thumbnail="https://s14-eu5.startpage.com/cgi-bin/serveimage?url=https%3A%2F%2Fi.ytimg.com%2Fvi%2F_1GLCEPezxk%2Fmaxresdefault.jpg&sp=3ac875bad39b3263c14e485a37d539e5&anticache=720150"
+      title="Hello There Again!"
+      preview-text="This is my second post."/>
+    <post-preview
+      id="3"
+      thumbnail="https://s14-eu5.startpage.com/cgi-bin/serveimage?url=https%3A%2F%2Fi.ytimg.com%2Fvi%2F_1GLCEPezxk%2Fmaxresdefault.jpg&sp=3ac875bad39b3263c14e485a37d539e5&anticache=720150"
+      title="Hi!"
+      preview-text="This is my third post."/>
+  </section>
+</template>
+
+<script>
+  import PostPreview from '~/components/Posts/PostPreview';
+
+  export default {
+    name: 'PostList',
+    components: {
+      PostPreview,
+    },
+  }
+</script>
+
+<style scoped>
+  .post-list {
+    display: flex;
+    padding: 20px;
+    box-sizing: border-box;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
