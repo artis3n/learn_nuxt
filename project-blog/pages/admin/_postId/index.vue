11e70e36282e55bcae5c1cca135c8b259ed59e50
Artis3n <dev@artis3nal.com>
2020-04-20 19:36:03 -0400

    Complete assignment 3

--- project-blog/pages/admin/_postId/index.vue
+++ project-blog/pages/admin/_postId/index.vue
@@ -7,21 +7,21 @@
 </template>
 
 <script>
+  import axios from 'axios'
+
   import AdminPostForm from '~/components/Admin/AdminPostForm'
+
   export default {
     layout: 'admin',
     components: {
       AdminPostForm
     },
-    data() {
-      return {
-        loadedPost: {
-          author: 'Artis3n',
-          title: 'The title of the post',
-          content: 'It is super amazing. Thanks for reading.',
-          thumbnailLink: 'https://s14-eu5.startpage.com/cgi-bin/serveimage?url=https%3A%2F%2Fi.ytimg.com%2Fvi%2F_1GLCEPezxk%2Fmaxresdefault.jpg&sp=3ac875bad39b3263c14e485a37d539e5&anticache=720150',
-        }
-      }
+    asyncData(context) {
+      return axios.get(`${process.env.FIREBASE_URL}/posts/${context.params.postId}.json`)
+        .then(res => ({
+          loadedPost: res.data,
+        }))
+        .catch(e => context.error(e))
     },
   }
 </script>
